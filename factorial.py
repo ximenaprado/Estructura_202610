34aed0c035e1c7fad1b1f0095127efd48255a374
Ximena Prado Blanco <41089488+ximenaprado@users.noreply.github.com>
2026-03-28 18:38:39 -0300

    Implement factorial function with error handling

--- factorial.py
+++ factorial.py
@@ -1,1 +1,9 @@
 
+def factorial(n):
+    if n < 0:
+        raise ValueError("El número no puede ser negativo")
+    if n == 0 or n == 1:
+        return 1
+    else:
+      return factorial(n-1)*n
+    
