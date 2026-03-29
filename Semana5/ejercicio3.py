2b2f545cef38bd4b61f3a3c534345d91d5df18e9
Ximena Prado Blanco <41089488+ximenaprado@users.noreply.github.com>
2026-03-29 20:53:14 -0300

    Add factorial functions in ejercicio3.py
    Implement iterative and recursive factorial functions.

---
+++ Semana5/ejercicio3.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 3:
+Dado un número entero positivo N, calcular su factorial.
+
+Debe implementar una versión iterativa y una recursiva.
+"""
+
+def factorial_ciclo(n):
+    pass
+
+
+def factorial_recursivo(n):
+    pass
