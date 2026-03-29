84a091427233dc67f4efa3ff33cd01c05970f38b
Ximena Prado Blanco <41089488+ximenaprado@users.noreply.github.com>
2026-03-29 20:54:55 -0300

    Create ejercicio4.py for counting even numbers
    Added a new Python file for Exercise 4 to count even numbers.

---
+++ Semana5/ejercicio4.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 4:
+Dado un número entero positivo N, contar cuántos números pares existen entre 1 y N.
+"""
+
+def contar_pares_ciclo(n):
+    pass
+
+
+def contar_pares_recursivo(n):
+    pass
